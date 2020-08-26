cbd9e3259604453ae5ff9c35da0ff51dd4551a47
Jim Crossley <jim@crossleys.org>
2020-08-26 15:57:21 -0400

    Following a questionable blinky tutorial
    This is the source from
    https://beta7.io/posts/embedded-rust-from-zero-to-blinky.html
    
    It's meant for a NUCLEO-F091RC board, which is not what I have.
    Subsequent commits will document what I changed to get it working with
    my board, a NUCLEO-F401RE.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+
+#[allow(unused)]
+use panic_halt;
+
+extern crate stm32f0xx_hal;
+use stm32f0xx_hal::{delay::Delay, prelude::*, stm32};
+
+use cortex_m::peripheral::Peripherals;
+use cortex_m_rt::entry;
+
+#[entry]
+fn main() -> ! {
+    if let (Some(mut p), Some(cp)) = (stm32::Peripherals::take(), Peripherals::take()) {
+        cortex_m::interrupt::free(move |cs| {
+            // Configure clock to 8 MHz (i.e. the default) and freeze it
+            let mut rcc = p.RCC.configure().sysclk(8.mhz()).freeze(&mut p.FLASH);
+
+            // (Re-)configure PA1 as output
+            let gpioa = p.GPIOA.split(&mut rcc);
+            let mut led = gpioa.pa1.into_push_pull_output(cs);
+
+            // Get delay provider
+            let mut delay = Delay::new(cp.SYST, &rcc);
+
+            // Toggle the LED roughly every second
+            loop {
+                led.toggle();
+                delay.delay_ms(1_000_u16);
+            }
+        });
+    }
+
+    loop {
+        continue;
+    }
 }
